1e893a166fa0757d4f49a588544f9dace246a4b7
Seoyoung-Mun <tjdud330@gmail.com>
2024-11-12 13:39:59 +0900

    fontSize조정으로 iconSize조정

--- lib/workout_guide_page.dart
+++ lib/workout_guide_page.dart
@@ -6,22 +6,27 @@ class WorkoutGuidePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('WorkoutGuide'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Text('스쿼트', style: Theme.of(context).textTheme.displaySmall),
-          Row(
-            children: [
-
-            ],
-          ),
-          Text('30분', style: Theme.of(context).textTheme.headlineLarge?.copyWith(color: Theme.of(context).colorScheme.secondary)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.play_circle_fill))
-        ],
-      )
-    );
+        appBar: AppBar(
+          title: Text('WorkoutGuide'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Text('스쿼트', style: Theme.of(context).textTheme.displaySmall),
+            Row(
+              children: [],
+            ),
+            Text('30분',
+                style: Theme.of(context)
+                    .textTheme
+                    .headlineLarge
+                    ?.copyWith(color: Theme.of(context).colorScheme.secondary)),
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.play_circle_fill),
+              iconSize: Theme.of(context).textTheme.headlineLarge?.fontSize,
+            )
+          ],
+        ));
   }
 }
