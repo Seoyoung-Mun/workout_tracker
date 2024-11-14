a01d8e20f4094fac7c62b2849af1d05be965881f
Seoyoung-Mun <tjdud330@gmail.com>
2024-11-14 13:41:50 +0900

    StatefulWidget

--- lib/workout_guide_page.dart
+++ lib/workout_guide_page.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
-class WorkoutGuidePage extends StatelessWidget {
+class WorkoutGuidePage extends StatefulWidget {
   WorkoutGuidePage({super.key});
-  final palyer = AudioPlayer();
 
+  @override
+  State<WorkoutGuidePage> createState() => _WorkoutGuidePageState();
+}
+
+class _WorkoutGuidePageState extends State<WorkoutGuidePage> {
+  final player = AudioPlayer();
+
+  IconButton getIconButton(){
+    if(player.state == PlayerState.playing){
+      return IconButton(
+        onPressed: () async{
+          await player.stop();
+          setState(() {});
+        },
+        icon: Icon(Icons.stop_circle),
+        iconSize: Theme.of(context).textTheme.displaySmall?.fontSize,
+        color: Theme.of(context).colorScheme.primary,
+      );
+    } else{
+      return IconButton(
+        onPressed: () async {
+          await player.play(AssetSource('sounds/squat.mp3'));
+          setState(() {});
+        },
+        icon: Icon(Icons.play_circle_fill),
+        iconSize: Theme.of(context).textTheme.displaySmall?.fontSize,
+        color: Theme.of(context).colorScheme.primary,
+    );
+    }
+
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +47,6 @@ class WorkoutGuidePage extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text('스쿼트', style: Theme.of(context).textTheme.displaySmall),
-
             Row(
               children: [
                 IconButton(
@@ -25,8 +55,7 @@ class WorkoutGuidePage extends StatelessWidget {
                   iconSize: Theme.of(context).textTheme.displayLarge?.fontSize,
                 ),
                 // Image(image: AssetImage('assets/squat.jpeg'),),
-                Expanded(
-                    child: Image.asset('assets/squat.jpeg')),
+                Expanded(child: Image.asset('assets/squat.jpeg')),
                 IconButton(
                   onPressed: () {},
                   icon: Icon(Icons.arrow_forward_ios),
@@ -34,19 +63,12 @@ class WorkoutGuidePage extends StatelessWidget {
                 ),
               ],
             ),
-
             Text('30분',
                 style: Theme.of(context)
                     .textTheme
                     .headlineLarge
                     ?.copyWith(color: Theme.of(context).colorScheme.secondary)),
-
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.play_circle_fill),
-              iconSize: Theme.of(context).textTheme.displaySmall?.fontSize,
-              color: Theme.of(context).colorScheme.primary,
-            )
+            getIconButton()
           ],
         ));
   }
