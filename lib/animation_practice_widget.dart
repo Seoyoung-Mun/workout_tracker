6f5546525cf133b9e29131e6348239f089d9bf1f
Seoyoung-Mun <tjdud330@gmail.com>
2024-12-05 13:50:15 +0900

    Opacity 효과반복

--- lib/animation_practice_widget.dart
+++ lib/animation_practice_widget.dart
@@ -9,13 +9,15 @@ class AnimationPracticeWidget extends StatefulWidget {
 }
 
 class _AnimationPracticeWidgetState extends State<AnimationPracticeWidget> {
+  double _begin = 0;
+  double _end = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: TweenAnimationBuilder(
-          duration: Duration(seconds: 3),
-          tween: Tween<double>(begin: 0, end: 1),
+          duration: Duration(seconds: 1),
+          tween: Tween<double>(begin: _begin, end: _end),
           builder: (context, value, child) {
             return Opacity(
               child: child, //여기서 child는 애니메이션을 적용시킬 위젯
@@ -29,7 +31,12 @@ class _AnimationPracticeWidgetState extends State<AnimationPracticeWidget> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          setState(() {
+            _begin = _begin == 0? 1:0;
+            _end = _end == 0? 1:0;
+          });
+        },
         child: Icon(Icons.play_arrow),
       ),
     );
