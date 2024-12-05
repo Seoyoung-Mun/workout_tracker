cdc0604d0a3c34e625eed2491847d203ba7547d0
Seoyoung-Mun <tjdud330@gmail.com>
2024-12-05 11:42:45 +0900

    feat: stop animated icon for dev

--- lib/animated_icon_widget.dart
+++ lib/animated_icon_widget.dart
@@ -15,13 +15,14 @@ class _AnimatedIconWidgetState extends State<AnimatedIconWidget>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<double> _animation;
+
   @override
   void initState() {
     super.initState();
     _controller =
         AnimationController(duration: const Duration(seconds: 1), vsync: this);
     final Animation<double> _curvedAnimation =
-        CurvedAnimation(parent: _controller, curve: Curves.easeOutSine);
+    CurvedAnimation(parent: _controller, curve: Curves.easeOutSine);
     _animation = TweenSequence<double>([
       TweenSequenceItem(tween: Tween(begin: 0.0, end: 40), weight: 1),
       TweenSequenceItem(tween: Tween(begin: 40.0, end: -20.0), weight: 1),
@@ -31,30 +32,46 @@ class _AnimatedIconWidgetState extends State<AnimatedIconWidget>
       TweenSequenceItem(tween: Tween(begin: 1.0, end: 0.0), weight: 1),
     ]).animate(_curvedAnimation);
 
-    _animation.addListener(() {// addListener는 애니메이션의 값이 변경될 때마다 호출되는 메서드
-      if(_animation.status == AnimationStatus.completed) {// 애니메이션이 완료되면 무한반복 하기 위해 다시 시작
-        _controller.reverse(); // reverse는 애니메이션을 뒤에서부터 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 뒤에서부터 시작
-      } else if(_animation.status == AnimationStatus.dismissed) {// 애니메이션이 처음부터 다시 시작되면
-        _controller.forward(); // forward는 애니메이션을 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 시작
+    _animation.addListener(() { // addListener는 애니메이션의 값이 변경될 때마다 호출되는 메서드
+      if (_animation.status ==
+          AnimationStatus.completed) { // 애니메이션이 완료되면 무한반복 하기 위해 다시 시작
+        _controller
+            .reverse(); // reverse는 애니메이션을 뒤에서부터 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 뒤에서부터 시작
+      } else if (_animation.status ==
+          AnimationStatus.dismissed) { // 애니메이션이 처음부터 다시 시작되면
+        _controller
+            .forward(); // forward는 애니메이션을 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 시작
       }
 
-      setState(() {// setState를 호출하면서 애니메이션의 값을 변경하면서 화면을 다시 그리도록 함
+      // setState(() {});// setState를 호출하면서 애니메이션의 값을 변경하면서 화면을 다시 그리도록 함
 
-      });
     });
 
-    _controller.forward(); // forward는 애니메이션을 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 시작
+    // _controller.forward(); // forward는 애니메이션을 시작하는 메서드. 이 메서드를 호출하면 애니메이션이 시작
+  }
+
+  @override
+  void dispose() { // dispose는 위젯이 사라질 때 호출되는 메서드. 애니메이션을 사용할 때는 반드시 dispose를 호출해야 함.
+    // _controller.dispose(); // dispose는 위젯이 사라질 때 호출되는 메서드. 애니메이션을 사용할 때는 반드시 dispose를 호출해야 함.
+    // TODO: implement dispose
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Transform.rotate(
-      angle: _animation.value * 0.0174533, // 0.0174533은 1도를 라디안 값으로 변환한 것
-      child: Icon(
-        widget.icon,
-        size: widget.size,
-        color: widget.color,
-      ),
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _animation.value * 0.0174533, // 0.0174533은 1도를 라디안 값으로 변환한 것
+          child: Icon(
+            widget.icon,
+            size: widget.size,
+            color: widget.color,
+          ),
+        );
+      },
+
     );
   }
 }
