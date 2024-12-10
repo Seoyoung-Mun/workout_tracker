a030b0b14685ce76f3d48a41b86be0050fa4f007
Seoyoung-Mun <tjdud330@gmail.com>
2024-12-10 11:16:31 +0900

    feat: FadeTransition

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -1,8 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class LandingPage extends StatelessWidget {
+class LandingPage extends StatefulWidget {
   const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage>
+    with SingleTickerProviderStateMixin {
+  //컨트롤러를 만들려면 Ticker 필요
+  late AnimationController _controller;
+  late Animation<double> _animation;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _animation = Tween<double>(begin: 0.0, end: 1.0).animate(_controller);
+    _controller.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,32 +32,38 @@ class LandingPage extends StatelessWidget {
         body: SafeArea(
             child: Container(
                 decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: AssetImage('assets/runner.png'),
-                        fit: BoxFit.cover,
-                        opacity: 0.2)),
+                  image: DecorationImage(
+                    image: AssetImage('assets/runner.png'),
+                    fit: BoxFit.cover,
+                    opacity: 0.2,
+                  ),
+                ),
                 child: SafeArea(
                   child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      // crossAxisAlignment: CrossAxisAlignment.stretch, //가로를 꽉 채우기
+                      mainAxisAlignment:
+                          MainAxisAlignment.spaceEvenly, // 세로를 가득 채우기
+                      // crossAxisAlignment: CrossAxisAlignment.stretch, //가로를 가득 채우기
                       children: [
                         // Container(
                         //   //빈 컨테이너로 위젯의 가로 확장
                         //   height: 0,
                         //   width: double.infinity, // 무한대로 넓히기
                         // ),
-                        Container(
-                          margin: EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 0),
-                          padding: EdgeInsets.all(8),
-                          width: double.infinity,
-                          height: 50,
-                          color: Colors.green[100],
-                          child: Text(
-                            '남들이 그만둘 때, 난 계속한다.',
-                            style: TextStyle(
-                                fontSize: 30, fontFamily: "NanumBrushScript"),
-                            textAlign: TextAlign.center,
+                        FadeTransition(
+                          opacity: _animation,
+                          child: Container(
+                            margin: EdgeInsets.symmetric(
+                                horizontal: 20, vertical: 0),
+                            padding: EdgeInsets.all(8),
+                            width: double.infinity,
+                            height: 50,
+                            color: Colors.green[100],
+                            child: Text(
+                              '남들이 그만둘 때, 난 계속한다.',
+                              style: TextStyle(
+                                  fontSize: 30, fontFamily: "NanumBrushScript"),
+                              textAlign: TextAlign.center,
+                            ),
                           ),
                         ),
                         SizedBox(
@@ -49,8 +76,8 @@ class LandingPage extends StatelessWidget {
                             Container(
                               child: Text(
                                 '환영합니다.',
-                                style: TextStyle(fontSize: 30, fontFamily: "Jua"),
-
+                                style:
+                                    TextStyle(fontSize: 30, fontFamily: "Jua"),
                               ),
                             ),
                             // Container(
@@ -70,8 +97,8 @@ class LandingPage extends StatelessWidget {
                               } /*콜백함수를 통해서 클릭함수 발생*/,
                               child: Text(
                                 'START',
-                                style: TextStyle(fontSize: 25, fontFamily: "Jua"),
-
+                                style:
+                                    TextStyle(fontSize: 25, fontFamily: "Jua"),
                               ),
                             ),
                           ],
