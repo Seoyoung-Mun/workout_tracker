dc9b470797dae0e7587e595e26cbf5bf8e9484f7
Seoyoung-Mun <tjdud330@gmail.com>
2025-01-02 11:38:38 +0900

    refactor: workoutData

--- lib/pages/my_workout_list_page.dart
+++ lib/pages/my_workout_list_page.dart
@@ -4,63 +4,26 @@ import 'package:workout_tracker/widgets/dashboard_card.dart';
 import 'package:workout_tracker/models/workout.dart';
 import 'package:go_router/go_router.dart';
 import 'package:workout_tracker/logic/workout_manager.dart';
+import 'package:workout_tracker/widgets/workout_tile.dart';
 
 class MyWorkoutListPage extends StatelessWidget {
   MyWorkoutListPage({super.key}) {
     WorkoutManager.increaseMonthlyWorkoutCount();
   } //constructor body는 인스턴스가 초기화 된 다음에 동작함
-
-  List<Widget> getWorkoutList(BuildContext context) {
-    final List<Workout> workouts = WorkoutManager
-        .workoutGroups[0].workouts; //instance 변수를 넣는 시점은 함수가 실행될 때 동작
-    //statefulWidget일 때는 Widget.변수 로 사용했었음.
-    List<Widget> workoutListRow = [];
-    for (int i = 0; i < workouts.length; i++) {
-      Workout workout = workouts[i]; // workouts 리스트에서 워크아웃을 하나씩 꺼내서
-      // 워크아웃 이름, 이미지, 시간을 변수로 저장
-      var name = workout.name;
-      var image = workout.imageName;
-      var time = workout.minutes;
-
-      workoutListRow.add(
-        GestureDetector(
-          onTap: () {
-            context.go('/workout_home/workout_list/0/workout_guide/$i');
-          },
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                margin: EdgeInsets.all(10),
-                width: 80,
-                height: 80,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/${image}'),
-                  ),
-                ),
-              ),
-              Expanded(
-                //화면의 크기와 상관없이 가능한 최대 크기로 확장
-                child: Text(
-                  '${i + 1}. ${name}',
-                  style: TextStyle(fontSize: 20),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 10.0),
-                child: Text('${time}분',
-                    style: TextStyle(fontSize: 20, color: Colors.blue)),
-              ),
-            ],
-          ),
-        ),
-      );
-    }
-    ;
-    return workoutListRow;
-  }
+  List<Workout> workouts = [
+    Workout(
+        name: '스쿼트',
+        minutes: 30,
+        imageName: 'squat.jpeg',
+        audioName: 'squat.mp3',
+        kcal: 200),
+    Workout(
+        name: '사이드런지',
+        minutes: 20,
+        imageName: 'side_lunge.jpeg',
+        audioName: 'side_lunge.mp3',
+        kcal: 100),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -69,11 +32,15 @@ class MyWorkoutListPage extends StatelessWidget {
         title: Text('MyWorkoutList'),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          ...getWorkoutList(context),
-          //빌드하면서 리스트에 추가하기 위해 '...'오퍼레이트 추가 (확장의 의미)
-        ],
+      body: ListView.builder(
+        itemCount: workouts.length,
+        itemBuilder: (context, index) {
+          return WorkoutTile(
+              index: index,
+              name: workouts[index].name,
+              image: workouts[index].imageName,
+              minutes: workouts[index].minutes);
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
---
+++ lib/widgets/workout_tile.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class WorkoutTile extends StatelessWidget {
+  final int index;
+  final String name;
+  final String image;
+  final int minutes;
+
+  WorkoutTile({
+    super.key,
+    required this.index,
+    required this.name,
+    required this.image,
+    required this.minutes,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          margin: EdgeInsets.all(10),
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage('assets/${image}'),
+            ),
+          ),
+        ),
+        Expanded(
+          //화면의 크기와 상관없이 가능한 최대 크기로 확장
+          child: Text(
+            '${index + 1}. ${name}',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(right: 10.0),
+          child: Text('${minutes}분',
+              style: TextStyle(fontSize: 20, color: Colors.blue)),
+        ),
+      ],
+    );
+  }
+}
