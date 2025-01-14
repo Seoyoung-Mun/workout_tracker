51a79e9d5ada92a2e07371dfad0c0cbb91f626f3
Seoyoung-Mun <tjdud330@gmail.com>
2025-01-14 14:31:21 +0900

    feat: context.watch

--- lib/pages/bloc_state/my_workout_list_page.dart
+++ lib/pages/bloc_state/my_workout_list_page.dart
@@ -51,13 +51,13 @@ class _MyWorkoutListPageState extends State<MyWorkoutListPage> {
               itemBuilder: (context, index) {
                 return WorkoutTile(
                   index: index,
-                  name: workouts[index].name,
-                  image: workouts[index].imageName,
-                  minutes: workouts[index].minutes,
-
-                  updateWorkoutDaysFromList: (Set<DaysOfWeek>? workoutDays) {
-                    workouts[index].workoutDays = workoutDays;
-                  },
+                  // name: workouts[index].name,
+                  // image: workouts[index].imageName,
+                  // minutes: workouts[index].minutes,
+                  //
+                  // updateWorkoutDaysFromList: (Set<DaysOfWeek>? workoutDays) {
+                  //   workouts[index].workoutDays = workoutDays;
+                  // },
                 );
               },
             );
--- lib/pages/bloc_state/workout_tile.dart
+++ lib/pages/bloc_state/workout_tile.dart
@@ -3,63 +3,58 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:workout_tracker/logic/workout_bloc/workout_bloc.dart';
 import 'package:workout_tracker/logic/workout_bloc/workout_event.dart';
 import 'package:workout_tracker/models/days_of_week.dart';
+import 'package:workout_tracker/models/workout.dart';
 import 'package:workout_tracker/pages/bloc_state/workout_day_selector.dart';
 
 class WorkoutTile extends StatelessWidget {
   final int index;
-  final String name;
-  final String image;
-  final int minutes;
-
-
-  final void Function(Set<DaysOfWeek>?) updateWorkoutDaysFromList;
 
   WorkoutTile({
     super.key,
     required this.index,
-    required this.name,
-    required this.image,
-    required this.minutes,
-
-    required this.updateWorkoutDaysFromList,
   });
+
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              margin: EdgeInsets.all(10),
-              width: 80,
-              height: 80,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: NetworkImage('${image}'),
+        Builder(
+          builder: (context) {
+            return Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: EdgeInsets.all(10),
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: NetworkImage('${context.watch<WorkoutBloc>().state.workouts[index].imageName}'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              //화면의 크기와 상관없이 가능한 최대 크기로 확장
-              child: Text(
-                '${index + 1}. ${name}',
-                style: TextStyle(fontSize: 20),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 10.0),
-              child: Text('${minutes}분',
-                  style: TextStyle(fontSize: 20, color: Colors.blue)),
-            ),
-            IconButton(
-                onPressed: () {
-                  context.read<WorkoutBloc>().add(DeleteWorkout(index));
-                },
-                icon: const Icon(Icons.delete_outline, size: 22))
-          ],
+                Expanded(
+                  //화면의 크기와 상관없이 가능한 최대 크기로 확장
+                  child: Text(
+                    '${index + 1}. ${context.watch<WorkoutBloc>().state.workouts[index].name}',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Text('${context.watch<WorkoutBloc>().state.workouts[index].minutes}분',
+                      style: TextStyle(fontSize: 20, color: Colors.blue)),
+                ),
+                IconButton(
+                    onPressed: () {
+                      context.read<WorkoutBloc>().add(DeleteWorkout(index));
+                    },
+                    icon: const Icon(Icons.delete_outline, size: 22))
+              ],
+            );
+          }
         ),
         WorkoutDaySelector(
           workoutIndex: index,
